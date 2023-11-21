79b6fe178bab1b3a31a0e1edad81a075644d36d1
khloeklaus <Khloeklaus2@gmail.com>
2023-11-21 19:05:02 +0400

    new changes4

---
+++ Sum.php
@@ -0,0 +1,18 @@
+
+<?php
+    echo "hello world <br>";
+    $many = 2.8888;
+    $many_2 = 2.1112;
+    $few=$many+$many_2;
+    print("$many+$many_2=$few<br>");
+    echo("Namussis<br>");
+    echo("Makato<br>");
+    if (true)
+    echo("I'm a mess<br>");
+    else
+    echo("Backdoor");
+    define ("MINISIZE",50);
+    echo MINISIZE;
+    echo constant("MINISIZE");
+    
+?>
