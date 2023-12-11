6e777900b083cd144fd654882772e5d66e0434d8
khloeklaus <khloeklaus2@gmail.com>
2023-12-11 17:11:24 +0300

    add comment in the connect file

--- includes/connect.php
+++ includes/connect.php
@@ -5,6 +5,7 @@ define("PASSWORD","");
 define("DATABASE","User_registration");
 $connection = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
 
+// this is used to connect to the database
 if(!$connection){
     die("connection failed" .mysqli_connect_error());
 }
